76eafdbf5258ec1a21d403d0a035afd8eee2bbf6
Paco <pacoswilliams@gmail.com>
2017-10-19 23:24:50 -0500

    fixed login bug and added attempt counter

--- Login.java
+++ Login.java
@@ -1,32 +1,33 @@
 public class Login extends Application {
   private String username;
   private String password;
-  private int attempts;
+  private int attempts = 3;
 
   public Login(String username, String password) {
     setUsername(username);
     setPassword(password);
   }
 
-  public boolean checkAccount(String message) {
+  public boolean checkAccount() {
     if (username.equals("mario") && password.equals("1234")) {
       return true;
     } else {
       return false;
     }
  }
-
+//char[] password = console.readPassword("Enter password");
+//Arrays.fill(password, ' ');
   public String loginAttempts() { //recursive function to test for login attempts
-        if (attempts > 3)
-          return "Try Again Later";
-        else if (attempts <= 3) {
-          setUsername(Lectura.readString("Username"));
-          setPassword(Lectura.readString("Password"));
-          checkAccount("Try Again" + "Attempts left: " + (attempts+1));
-          attempts++;
+        if (checkAccount())
+          return "Success!";
+        else if (attempts > 0) {
+          System.out.println("Attempts: " + (attempts));
+          setUsername(Lectura.readString("Username")); setPassword(Lectura.readString("Password")); //
+          checkAccount();
+          attempts--;
           return loginAttempts();
         } else
-          return "Success!";
+          return "Try Again Later";
       }
   public String toString() {
     return loginAttempts();
