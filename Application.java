5333c83d8f40b44532b5263c74850acdd0e5f3f1
Paco <pacoswilliams@gmail.com>
2017-10-19 23:25:31 -0500

    deleted test outputs

--- Application.java
+++ Application.java
@@ -1,8 +1,9 @@
 import java.io.*;
+
 class Application {
   public static void main(String args[]) {
-    System.out.print("hello world");
-    Login toby = new Login("toby","gay123");
+    System.out.println("Login:");
+    Login toby = new Login("toby", "gay123");
     System.out.println(toby);
   }
 }
