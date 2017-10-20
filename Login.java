46d698d654003468604e3e9bcdcde6accf43e480
Paco <pacoswilliams@gmail.com>
2017-10-19 23:47:03 -0500

    added status attribute to login.

--- Login.java
+++ Login.java
@@ -2,10 +2,12 @@ public class Login extends Application {
   private String username;
   private String password;
   private int attempts = 3;
+  protected boolean status;
 
-  public Login(String username, String password) {
+  public Login(String username, String password, boolean status) {
     setUsername(username);
     setPassword(password);
+    setStatus(status);
   }
 
   public boolean checkAccount() {
@@ -18,8 +20,10 @@ public class Login extends Application {
 //char[] password = console.readPassword("Enter password");
 //Arrays.fill(password, ' ');
   public String loginAttempts() { //recursive function to test for login attempts
-        if (checkAccount())
+        if (checkAccount()) {
+          setStatus(true); //log in status to true;
           return "Success!";
+        }
         else if (attempts > 0) {
           System.out.println("Attempts: " + (attempts));
           setUsername(Lectura.readString("Username")); setPassword(Lectura.readString("Password")); //
@@ -34,6 +38,7 @@ public class Login extends Application {
   }
 
   public void setUsername(String username) {this.username = username;}
+  public void setStatus(boolean status) {this.status = status;}
   public void setPassword(String password) {this.password = password;}
   public String username() {return username;}
   public String password() {return password;}
