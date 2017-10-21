12a1045666bc2426e9a167c3e954514f596bd9a6
Paco <pacoswilliams@gmail.com>
2017-10-21 01:43:07 -0500

    JSON writing and reading functionally working 100%

--- Json.java
+++ Json.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
@@ -7,46 +8,34 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 class Json {
-  /*  public static void main(String args[]) {
-      //SONParser parser = new JSONParser();
-
-    }
-  }
-  */
   public Json() {
-    
   }
   public JSONArray getData() {
-    JSONParser parser = new JSONParser();
+    JSONParser parser = new JSONParser(); //used to turn JSON to string
     JSONArray array = null;
     try {
-         //Object obj = parser.parse(new FileReader("C:\\Users\\paco\\Documents\\GitHub\\proyectoFinal\\data\\data.json"));
-         //JSONArray jArray = obj.getJSONArray();
-         array = (JSONArray)parser.parse(new FileReader("C:\\Users\\paco\\Documents\\GitHub\\proyectoFinal\\data\\data.json"));
-         //creating a new JSON object with input data
-
-         //JSONArray jsonArray = new JSONArray();
-         //System.out.print(array.get(1).get("city"));
-         for (int i = 0; i < 3;i++) {
-            System.out.println((array.get(i)));
-          }
-         }
-      catch (FileNotFoundException e) {e.printStackTrace();}
+         array = (JSONArray)parser.parse(new FileReader("C:\\Users\\paco\\Documents\\GitHub\\proyectoFinal\\data\\data.json")); // reads from exisiting file
+    }
+      catch (FileNotFoundException e) {e.printStackTrace();} //error catching
      catch (IOException e) {e.printStackTrace();}
     catch (ParseException e) {e.printStackTrace();}
     return array;
   }
-  public JSONObject addProduct() {
-    JSONObject product = new JSONObject();
-  //  product.put
-    product.put("city","New York");
-    product.put("job","teacher");
-    return product;
-  }
-
-  public String toString() {
+  //
+  public void addProduct() { //adds object to array and saves it to file
     JSONArray data = getData();
-    return data.get(1) + "";
-
+    JSONObject product = new JSONObject(); //new object to add
+    product.put("city","New York"); //object fields (attributes)
+    product.put("job","teacher");
+    data.add(product); //add object to array
+    try (FileWriter file = new FileWriter("C:\\Users\\paco\\Documents\\GitHub\\proyectoFinal\\data\\data.json")) { //write updated array to file
+  			file.write(data.toJSONString());
+  		}
+      catch (FileNotFoundException e) {e.printStackTrace();}
+     catch (IOException e) {e.printStackTrace();}
+  }
+  public String toString() {
+    addProduct();
+    return "";
   }
 }
