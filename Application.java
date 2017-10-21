1335c372a094ab9670ec75e963ce4c75fd29ac8d
Paco <pacoswilliams@gmail.com>
2017-10-20 23:32:21 -0500

    modified main renamed jar.

--- Application.java
+++ Application.java
@@ -3,7 +3,9 @@ import java.io.*;
 class Application {
   public static void main(String args[]) {
     System.out.println("Login:");
-    Login toby = new Login();
-    System.out.println(toby);
+    //Login toby = new Login();
+    Json test = new Json();
+    System.out.println(test);
+    //System.out.println(toby);
   }
 }
