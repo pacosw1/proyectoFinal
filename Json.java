67f458d3efac1e96177bd2013fc97d3235c344fe
pacosw1 <pacoswilliams@gmail.com>
2017-10-27 14:24:18 -0500

    fixed exceptions

--- Json.java
+++ Json.java
@@ -14,18 +14,24 @@ class Json {
   public JSONArray getData() {
     JSONParser parser = new JSONParser(); //used to turn JSON to string
     JSONArray array = new JSONArray();
+    FileReader file = null;
     try {
-         array = (JSONArray)parser.parse(new FileReader("C:\\Users\\"+names+"\\Documents\\GitHub\\proyectoFinal\\data\\data.json")); // reads from exisiting file
+         file = new FileReader("C:\\Users\\"+names+"\\Documents\\GitHub\\proyectoFinal\\data\\data.json");
+
+         array = (JSONArray)parser.parse(file); // reads from exisiting file
+         file.close();
          //System.out.print(array.get(1).get("city"));
       }
       catch (FileNotFoundException e) {e.printStackTrace();} //error catching
      catch (IOException e) {e.printStackTrace();}
     catch (ParseException e) {e.printStackTrace(); }
+    finally{}
     return array;
   }
   //
   public void addProduct(String name, String size, String price, String quantity, String temp, String timestamp) { //adds object to array and saves it to file
     JSONArray data = getData();
+    FileWriter file = null;
     JSONObject product = new JSONObject(); //new object to add
     product.put("Temp", temp);
     product.put("Quantity", quantity);
@@ -36,11 +42,14 @@ class Json {
     product.put("timestamp", timestamp);
     product.put("payment-type","cash");
     data.add(product); //add object to array
-    try (FileWriter file = new FileWriter("C:\\Users\\"+names+"\\Documents\\GitHub\\proyectoFinal\\data\\data.json")) { //write updated array to file
+    try { //write updated array to file
+        file = new FileWriter("C:\\Users\\"+names+"\\Documents\\GitHub\\proyectoFinal\\data\\data.json");
   			file.write(data.toJSONString());
+        file.close();
   		}
       catch (FileNotFoundException e) {e.printStackTrace();}
      catch (IOException e) {e.printStackTrace();}
+     //finally{}
   }
 
 }
