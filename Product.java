9e6873e576fb4aa0fd4f8e869a2c0d17df263472
MoonMoon98 <32946975+MoonMoon98@users.noreply.github.com>
2017-11-09 12:51:11 -0600

    Order

--- Product.java
+++ Product.java
@@ -1,39 +1,37 @@
 import java.io.*;
 import java.util.*;
-class Product implements Serializable{
+class Product implements Serializable{//Archive
   protected String code;
   protected double price;
   protected int quantity;
+  //Construct
+  	public Product(double price,String code,int quantity) {
+  		setCode(code);
+  		setPrice(price);
+      setQuantity(quantity);
+  	}
+  //Set
+  	public void setCode(String code) {
+  		this.code = code;
+  	}
 
-	public Product(double price,String code,int quantity) {
-		this.code = code;
-		this.price = price;
-    this.quantity = quantity;
-	}
+  	public void setPrice(double price) {
+  		this.price = price;
+  	}
 
+  	public void setQuantity(int quantity) {
+  		this.quantity = quantity;
+  	}
+  //Get
+  	public String getCode() {
+  		return code;
+  	}
 
+  	public double getPrice() {
+  		return price;
+  	}
 
-	public String getCode() {
-		return code;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+  	public int getQuantity() {
+  		return quantity;
+  	}
 }
--- Tea.java
+++ Tea.java
@@ -1,8 +1,14 @@
 class Tea extends Drink{
   //Construct
     public Tea(String size){
-      super();
+      super(size);
     }
   //Set
-
+    public void setSize(String size){
+      this.size = size;
+    }
+  //Get
+    public String getSize(){
+      return size;
+    }
 }
