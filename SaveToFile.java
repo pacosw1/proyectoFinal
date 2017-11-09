46fc89da97f7e0197c36dbcdcc6645a1e7afc4e7
Paco <pacoswilliams@gmail.com>
2017-11-08 18:16:11 -0600

    Added reading and writing functionality to transaction and ingredient files

--- SaveToFile.java
+++ SaveToFile.java
@@ -24,6 +24,25 @@ class SaveToFile implements Serializable {
         }
         return objectsList;
     }
+    public ArrayList<Transaction> readTransactions(String path) {
+      FileInputStream fis = null;
+      ArrayList<Transaction> objectsList = new ArrayList<Transaction>(); //Ingredient specified as type
+      boolean cont = true;
+      try{
+         fis = new FileInputStream(path);
+         ObjectInputStream input = new ObjectInputStream(fis);
+         while(cont){
+            Transaction obj = (Transaction)input.readObject(); //reads objects from file and adds them to an arraylist which is then returned
+            if(obj != null)
+               objectsList.add(obj);
+            else
+               cont = false;
+         }
+        }catch(Exception e){
+           //System.out.println(e.printStackTrace());
+        }
+        return objectsList;
+    }
     public void saveIngredient(Ingredient[] ingredients, String path) { //saves ingredient object to .dat file.
 
       ArrayList<Ingredient> current = readIngredients(path); //uses the read method tu get all saved objects from the file.
@@ -51,14 +70,21 @@ class SaveToFile implements Serializable {
         catch(IOException e) {System.out.println("Error al cerrar el archivo");}
       }
     }
-    public void saveTransaction(Transaction obj, String path) { //saves object to an object .dat file for transactions;
-      obj = null;
+    public void saveTransaction(ArrayList<Transaction> obj, String path) { //saves object to an object .dat file for transactions;
+      ArrayList<Transaction> current = readTransactions(path); //uses the read method tu get all saved objects from the file.
+      for (int j = 0;j< obj.size();j++) //adds the ingredient array to arraylist.
+      {
+        current.add(obj.get(j));
+      }
       FileOutputStream f1 = null;
       ObjectOutputStream f2=null;
       try {
         f1 = new FileOutputStream(path);
         f2 = new ObjectOutputStream(f1);
-        f2.writeObject(obj);
+        for (int j = 0;j< current.size();j++) //writes all old and new Objects to file from the arrayList;
+        {
+          f2.writeObject(current.get(j));
+        }
       }
       catch(IOException e) {System.out.println("No se almaceno");}
       finally {
