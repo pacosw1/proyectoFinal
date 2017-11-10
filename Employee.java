db721606d4b91e6a96650e374a4b6e62a97aff12
Moon <marioluna98@yahoo.com.mx>
2017-11-09 22:54:58 -0600

    Employee and Manager

--- Employee.java
+++ Employee.java
@@ -1,15 +1,39 @@
 class Employee {
 protected double salary;
+protected double hours;
+protected String name;
 //Construct
-public Employee(double salary){
+public Employee(double salary, double hours, String name){
         setSalary(salary);
+        setName(name);
+        setHours(hours);
 }
 //Set
 public void setSalary(double salary){
         this.salary = salary;
 }
+public void setHours(double hours){
+        this.hours = hours;
+}
+public void setName(String name){
+        this.name = name;
+}
 //Get
 public double getSalary(){
         return salary;
+}
+public double getHours(){
+        return hours;
+}
+public String getName(){
+        return name;
+}
+//Methods
+public double payment(){
+  return hours * salary;
+}
+//Display
+public String toString(){
+  return "Salary:" + hours;
 }
 }
--- Manager.java
+++ Manager.java
@@ -1,14 +1,50 @@
 class Manager extends Employee {
+  private String numberEmployees;
 //Construct
-public Manager(double salary){
-        super(salary);
+public Manager(double salary, double hours, String name, String numberEmployees){
+        super(salary, hours, name);
+        setNumberEmployees(numberEmployees);
 }
 //Set
 public void setSalary(double salary){
         this.salary = salary;
 }
+public void setHours(double hours){
+        this.hours = hours;
+}
+public void setName(String name){
+        this.name = name;
+}
+public void setNumberEmployees(String numberEmployees){
+  this.numberEmployees = numberEmployees;
+}
 //Get
 public double getSalary(){
         return salary;
+}
+public double getHours(){
+        return hours;
+}
+public String getName(){
+        return name;
+}
+public String getNumberEmployees(){
+        return numberEmployees;
+}
+//Methods
+public double payment(){
+  return hours * salary;
+}
+public String employeeMonth(){
+/*  int x = Integer.parseOf(numberEmployees);
+  String[] employeeArray = new double[x];
+  Employee employee = new Employee()
+  for(int i = 0; i < x; i++){
+  Manager should be Father class
+  }*/
+  return "Employee of Month:"
+}
+public String accessReports(){
+  return "Reports";
 }
 }
