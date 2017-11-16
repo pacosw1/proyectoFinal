e498e2c7959e951cc62548523aa9bfd4d12a52eb
MoonMoon98 <32946975+MoonMoon98@users.noreply.github.com>
2017-11-16 17:11:10 -0600

    Starting sales proyections

---
+++ SalesProyections.java
@@ -0,0 +1,18 @@
+class SalesProyections{
+  private double expenses;//Spending that varies(I.e. Inventory)
+  private double costs;//Fixed Spending(I.e. Salary)
+  private double sales;
+  public SalesProyections(){
+
+  }
+  //Methods
+  public double equilibriumPoint(){//Sales needed to cover expenses/costs
+    double total = 0;
+    //Add all expenses
+    //total = Expenses added/(grossMargin/100)
+    return total;
+  }
+  public double grossMargin(){//Later has to be converted into percentage
+    return (sales - (expenses + costs))/sales;
+  }
+}
