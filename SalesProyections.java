118517dc24dad2e2971e8a0b030b4a9f92f4664b
MoonMoon98 <32946975+MoonMoon98@users.noreply.github.com>
2017-11-16 19:19:56 -0600

    Implementing Sales Proyections

--- SalesProyections.java
+++ SalesProyections.java
@@ -1,18 +1,92 @@
+import java.io.*;
+/*
+  main
+  double[][] currentSales = new double[][];
+  double[][] predefinedSales = new double[][];
+  byte more = 0;
+  do{
+  System.out.println("Past Coffee Sales:");//PredefinedSales
+  double predefinedCoffeeSales = Lectura.readDouble();
+  System.out.println("Past Coffee Sales:");
+  double predefinedChocolateSales = Lectura.readDouble();
+  System.out.println("Past Coffee Sales:");
+  double predefinedTeaSales = Lectura.readDouble();
+  System.out.println("Past Coffee Sales:");
+  double predefinedOthersSales = Lectura.readDouble();
+  //Current Sales
+  Cafe.sales();
+  Chocolate.sales();
+  Tea.sales();
+  Others.sales();
+  int coffeBuyers;
+  int chocolateBuyers;
+  int teaBuyers;
+  int otherBuyers;
+  System.out.println("Want to register more? 1 yes, 0 no");
+  more = Lectura.readByte();
+}while(more == 1);
+*/
 class SalesProyections{
-  private double expenses;//Spending that varies(I.e. Inventory)
-  private double costs;//Fixed Spending(I.e. Salary)
-  private double sales;
-  public SalesProyections(){
-
+  private long expectedTime;//Time to sell x product
+  private double economyGrowthRate;//Changes the cost of dollars
+  private Inventory inventoryProducts;
+  private double[][] currentSales;//Sales per product, Products per Buyer
+  private double[][] predefinedSales;
+  //Construct
+  public SalesProyections(long expectedTime, double economyGrowthRate, Inventory inventoryProducts,
+  double[][] currentSales, double[][] predefinedSales){
+    setExpectedTime(expectedTime);
+    setEconomyGrowthRate(economyGrowthRate);
+    setInventoryProducts(inventoryProducts);
+    setCurrentSales(currentSales);
+    setPredefinedSales(predefinedSales);
+  }
+  //Set
+    public void setExpectedTime(long expectedTime){
+        this.expectedTime = expectedTime;
+    }
+    public void setEconomyGrowthRate(double economyGrowthRate){
+        this.economyGrowthRate = economyGrowthRate;
+    }
+    public void setInventoryProducts(Inventory inventoryProducts){
+        this.inventoryProducts = inventoryProducts;
+    }
+    public void setCurrentSales(double[][] currentSales){
+        this.currentSales = currentSales;
+    }
+    public void setPredefinedSales(double[][] predefinedSales){
+        this.predefinedSales = predefinedSales;
+    }
+  //Get
+  public long getExpectedTime(){
+        return expectedTime;
+  }
+  public double getEconomyGrowthRate(){
+        return economyGrowthRate;
+  }
+  public Inventory getInventoryProducts(){
+        return inventoryProducts;
+  }
+  public double[][] getCurrentSales(){
+        return currentSales;
+  }
+  public double[][] getPredefinedSales(){
+        return predefinedSales;
   }
   //Methods
-  public double equilibriumPoint(){//Sales needed to cover expenses/costs
-    double total = 0;
-    //Add all expenses
-    //total = Expenses added/(grossMargin/100)
-    return total;
+  public double salesPerClient(){
+    return 2.3;
   }
-  public double grossMargin(){//Later has to be converted into percentage
-    return (sales - (expenses + costs))/sales;
+  public double totalSalesCompany(){
+    return 2.2;
+  }
+  public double totalSalesProduct(){
+    return 5;
+  }
+  public double changeOfSales(){
+    return 5.8;
+  }
+  public String salesTendencies(){//How much has a product sold, how much is left on the inventory, what to do.
+    return 3.3;
   }
 }
